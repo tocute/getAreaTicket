fb4e11c0e9554af83f6d8486b2ae0b4c77e3d468
tocute <tocute520@gmail.com>
2014-11-07 18:57:43 +0800

    modify ui

--- js/main.js
+++ js/main.js
@@ -1,61 +1,58 @@
 $(document).on("mobileinit",function(e)
 {
-	
+    
 });
 
 $(document).on("pageinit","#page-1",function(e)
 {
-	Parse.initialize("CPFQXuoHErkxiN8b3uDFuGuGBnZbLen9jglvAB4p", "qvHqLCDitrqkwAL3bSbMfbdcYlUY9wlfVJZmom3S");
-		
-	var checkFieldFormat = function ()
-	{
-		var is_valid = true;
-		if(!$.isNumeric($("#area_id").val()))
-		{
-			$("#area_id").val("");
-			is_valid = false;
-		}	
-		return is_valid;
-	}
-	
-	var onBtnSubmit2Click = function (e)
-	{
-		var area_id = $("#area_id").val();
-		
-		if(area_id != undefined)
-		{
-			if(checkFieldFormat())
-			{
-				Parse.Cloud.run('GetAreaTicket', 
-					{ areaId: area_id}, 
-					{
-	  					success: function(result) 
-	  					{
-    						// result is 'Hello world!'
-    						$("#popupText").html("Area : "+ result.areaId+" 白："+result.whiteTicketNum +" \n 藍："+result.blueTicketNum);
-    					},
-  						error: function(error) 
-  						{
-  							/*{
-								"code":141,
-								"message":"密碼錯誤  請再次確認投票所編號與密碼"
-							}*/
-							$("#popupText").html("");
-  							alert(error.message);
-  						}
-					});
-			}
-			else
-			{
-				alert("格式錯誤");
-			}
-		}	
-		else	
-		{
-			alert("有欄位尚未填寫");
-		}
-	}
+    Parse.initialize("CPFQXuoHErkxiN8b3uDFuGuGBnZbLen9jglvAB4p", "qvHqLCDitrqkwAL3bSbMfbdcYlUY9wlfVJZmom3S");
+        
+    var checkFieldFormat = function ()
+    {
+        var is_valid = true;
+        if(!$.isNumeric($("#area_id").val()))
+        {
+            $("#area_id").val("");
+            is_valid = false;
+        }   
+        return is_valid;
+    }        
 
-	$("#btn_submit").on("click",onBtnSubmit2Click);
+    var onBtnGetTicketClick = function (e)
+    {
+        var area_id = $("#area_id").val();
+        
+        if(area_id == undefined)
+        {
+            alert("有欄位尚未填寫");
+            return;
+        }
+        if(!checkFieldFormat())
+        {
+            alert("格式錯誤");
+            return;
+        }
+
+        var temp_list = $("#ticketList");
+        temp_list.empty();
+        Parse.Cloud.run("GetAreaTicket",
+            { areaId: ""+area_id}, 
+            {
+                success: function(result) 
+                {
+                    temp_list.append("<li>"+"Area : "+ result.areaId+" 白："+result.whiteTicketNum +" \n 藍："+result.blueTicketNum+"</li>");
+                    temp_list.listview("refresh");
+                },
+                error: function(error) 
+                {
+                    /*{
+                        "code":141,
+                        "message":"密碼錯誤  請再次確認投票所編號與密碼"
+                    }*/
+                }
+            });  
+    }
+
+    $("#btn_submit").on("click",onBtnGetTicketClick);
 
 });
